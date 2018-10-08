9f786acac16d45b6089c61b371a8c6b037e373da
rwanda-ether <rwanda-ether@localhost>
2018-10-08 11:18:26 +0900

    Don't spend time beating on a wall, hoping to transform it into a door. -- Coco Chanel

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # web3
+
+This repository is for studying web3 API using python, javascript, golang etc.
+For discussing, please use https://rwanda-ether.slack.com/
+
---
+++ web3.py/example-001.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+import web3
+
+if __name__ == "__main__":
+
+    provider = web3.HTTPProvider('http://192.168.1.2:18545') # ropsten node
+    www3 = web3.Web3(provider)
+
+    wei = www3.eth.getBalance("0x81b7E08F65Bdf5648606c89998A9CC8164397647")
+    eth = www3.fromWei(wei, "ether")
+    bnum = www3.eth.blockNumber
+    print(bnum, wei, eth)
+
+
+
