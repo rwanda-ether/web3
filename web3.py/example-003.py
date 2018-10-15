1d5f818a67f8dae709d0b24702dbc615a879ce76
rwanda-ether <rwanda-ether@localhost>
2018-10-15 13:18:53 +0900

    Life is not fair, get used to it. -- Bill Gates

--- web3.py/example-003.py
+++ web3.py/example-003.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-#Time-stamp: <Mon Oct 15 12:00:22 JST 2018 hamada>
+#Time-stamp: <Mon Oct 15 13:18:44 JST 2018 hamada>
 '''
 Example: an analysys of the top miners
 '''
@@ -12,11 +12,11 @@ def puts_block(block):
         print (key, block[key])
 
 if __name__ == "__main__":
-    geth_node = 'http://117.102.189.70:18545'  # ropsten node, public primary
-    geth_node = 'http://192.168.103.200:18545' # ropsten node, private
-    geth_node = 'http://117.102.189.70:28545'  # ropsten node, public secondary
-    geth_node = 'http://192.168.103.201:28545' # ropsten node, private
-    geth_node = 'http://192.168.103.202:18545' # ropsten node, private
+    geth_node = 'http://117.102.189.70:18545'  # ropsten node, public primary # Constantinople
+    geth_node = 'http://117.102.189.70:28545'  # ropsten node, public secondary # Byzantine
+    geth_node = 'http://192.168.103.200:18545' # ropsten node, private # Constantinople
+    geth_node = 'http://192.168.103.202:18545' # ropsten node, private # Constantinople
+    geth_node = 'http://192.168.103.201:28545' # ropsten node, private # Byzantine
 
 
 
@@ -41,7 +41,7 @@ if __name__ == "__main__":
 
         miner_count = {}
 
-        nblock = 100
+        nblock = 1000
         t_next = block_latest['timestamp']
 
         for i in range(nblock):
@@ -62,12 +62,15 @@ if __name__ == "__main__":
 
         miner_list = sorted(miner_count.items(), key=lambda x: -x[1])
         print ()
-        print ("-------------------------------------------")
-        print (" %d miners exist in the last %d blocks." % (len(miner_list), nblock))
-        print ("-------------------------------------------")
-        print ()
+
+        print ("----------------------------------------------------------------------")
+        print (" %d miners won in the last %d blocks." % (len(miner_list), nblock))
+        print (" Range of blocks: %d  - %d" % (bnum_latest - nblock, bnum_latest-1))
+        print ("----------------------------------------------------------------------")
+        print ("Account,                                   count (ratio): balance")
+        print ("----------------------------------------------------------------------")
         for k, v in miner_list:
             ratio = v / (nblock * 1.) 
             adr = www3.toChecksumAddress(k) 
             ballance = www3.fromWei(www3.eth.getBalance(adr), "ether")
-            print ("%s, %d, (%s): %f" %(adr, v, '{:.2%}'.format(ratio), ballance))
+            print ("%s, %d (%s): %f" %(adr, v, '{:.2%}'.format(ratio), ballance))
